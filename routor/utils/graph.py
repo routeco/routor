1ce3f47c2b64f7a1252f12bce0e2531a7534a253
escaped <escaped@users.noreply.github.com>
2021-01-07 01:08:38 +0000

    chore: update project structure to 78c5496a

--- routor/utils/graph.py
+++ routor/utils/graph.py
@@ -4,9 +4,11 @@ import networkx
 from osmnx.graph import graph_from_xml
 
 
-def load_map(map: Path) -> networkx.DiGraph:
+def load_map(map_path: Path) -> networkx.DiGraph:
     """
     Load graph from a .osm.xml file.
     """
-    graph = graph_from_xml(map, bidirectional=False, simplify=False, retain_all=False)
+    graph = graph_from_xml(
+        map_path, bidirectional=False, simplify=False, retain_all=False
+    )
     return graph
